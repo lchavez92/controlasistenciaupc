691245c6c987d2f6dd45bafccb1f731910326d62
Jefferson <u201825299@upc.edu.pe>
2021-08-25 17:42:38 -0500

    Borrado "Menu registrar solicitud"

--- app/src/main/java/pe/upc/controlasistencias/ui/registroasistencia/RegistroasistenciaViewModel.java
+++
@@ -1,7 +0,0 @@
-package pe.upc.controlasistencias.ui.registroasistencia;
-
-import androidx.lifecycle.ViewModel;
-
-public class RegistroasistenciaViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
